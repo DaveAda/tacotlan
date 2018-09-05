952052d1b1027e4a592b668751188c6ee0a0153b
Dave Ada <davealyinovich@gmail.com>
2018-09-04 20:44:01 -0500

    Instagram feed working, need to edit hover events

--- js/main.js
+++ js/main.js
@@ -8,9 +8,16 @@ $(document).ready(function () {
  });
 });
 
-var feed = new Instafeed({
-	get: "tagged",
-	tagName: "chicago",
-	clientId: "67011a8a795d429d9bad9262b997692e"	
+$(document).ready(function(){
+var userFeed = new Instafeed({
+	get: 'user',
+	userId: '1398104997',
+	limit: 4,
+	resolution: 'standard_resolution',
+	accessToken: '1398104997.1677ed0.7787f1510c6449b1b4e1913cfbdb036a',
+	sortBy: 'most-recent',
+	template: '<div class="gallery"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
 })
-feed.run();
+userFeed.run();
+});
+
